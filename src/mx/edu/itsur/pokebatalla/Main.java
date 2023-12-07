8b29832c3040191303dea46077798500312c5969
WatePooet <91634784+WatePooet@users.noreply.github.com>
2023-12-07 12:10:00 -0600

    Se actualiza el Main.java

--- src/mx/edu/itsur/pokebatalla/Main.java
+++ src/mx/edu/itsur/pokebatalla/Main.java
@@ -1,44 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package mx.edu.itsur.pokebatalla;
 
+import mx.edu.itsur.pokebatalla.model.Pokemons.Pikachu;
+import mx.edu.itsur.pokebatalla.model.Pokemons.Charmander;
+import mx.edu.itsur.pokebatalla.model.Pokemons.Raichu;
+import mx.edu.itsur.pokebatalla.model.Pokemons.Pidgeot;
+import mx.edu.itsur.pokebatalla.model.Pokemons.Gyarados;
+import mx.edu.itsur.pokebatalla.model.Pokemons.Blastoise;
 import mx.edu.itsur.pokebatalla.model.battles.Batalla;
 import mx.edu.itsur.pokebatalla.model.battles.Entrenador;
-import mx.edu.itsur.pokebatalla.model.pokemons.Bullbasaur;
-import mx.edu.itsur.pokebatalla.model.pokemons.Pikachu;
-import mx.edu.itsur.pokebatalla.model.pokemons.Charmander;
 
 /**
  *
- * @author FJML1983
+ * @author Francisco Gonzalez Regalado
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        System.out.println("Hello PokeBatalla!");
-        
-        //Instanciar pokemons
-        Pikachu pika = new Pikachu();
-        Charmander charm = new Charmander();
-        Bullbasaur bullb = new Bullbasaur();
-        
-        //Instanciar entrenadores y que capturen pokemons
-        Entrenador ent1 = new Entrenador("Hannia");
-        ent1.capturarPokemon(pika);
-        ent1.capturarPokemon(bullb);
 
-        Entrenador ent2 = new Entrenador("Stacy");
-        ent2.capturarPokemon(charm);
+        System.out.println("Hola Pokemon\n");
+
+        Charmander chr = new Charmander();
+        Raichu rai = new Raichu();
+        Pidgeot pid = new Pidgeot();
+        Gyarados gya = new Gyarados();
+        Pikachu pik = new Pikachu();
+        Blastoise blas = new Blastoise();
         
-        //Instanciar batalla e iniciarla.
-        Batalla b = new Batalla(ent1, ent2);
-        b.desarrollarBatalla();
+        Entrenador ent1 = new Entrenador("RED");
+        ent1.capturarPokemon(chr);
+        ent1.capturarPokemon(rai);
+        ent1.capturarPokemon(pid);
         
+        Entrenador ent2 = new Entrenador("BLUE");
+        ent2.capturarPokemon(gya);
+        ent2.capturarPokemon(pik);
+        ent2.capturarPokemon(blas);
+        
+        Batalla a = null;
+        
+        Batalla batallaGuardada = FileManager.leerDesdeArchivo();
+        
+        if(batallaGuardada != null){
+            a = batallaGuardada;
+        } else {
+            a = new Batalla(ent1, ent2);                 
+        }
+        a.desarrollarBatalla();
     }
-    
 }
