0243cac7610f5f8c4116a1b7121de1b95f87ce8b
WatePooet <91634784+WatePooet@users.noreply.github.com>
2023-12-07 12:10:57 -0600

    Se agrega el controlador de archivos

---
+++ src/mx/edu/itsur/pokebatalla/FileManager.java
@@ -0,0 +1,40 @@
+package mx.edu.itsur.pokebatalla;
+
+import mx.edu.itsur.pokebatalla.model.battles.Batalla;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author 
+ */
+public class FileManager {
+
+    public static final String FILE_NAME = "CombateGuardado.txt";
+
+    public static void guardarHaciaArchivo(Batalla batalla) {
+        try {
+            FileOutputStream flujoSalida = new FileOutputStream(FILE_NAME);
+            ObjectOutputStream escritorDeObjetos = new ObjectOutputStream(flujoSalida);
+            escritorDeObjetos.writeObject(batalla);
+        } catch (FileNotFoundException ex) {
+            System.out.println("Archivo no creado");
+        } catch (IOException ex) {
+            
+        }
+    }
+
+    public static Batalla leerDesdeArchivo() {
+        try {
+            FileInputStream flujoEntrada = new FileInputStream(FILE_NAME);
+            ObjectInputStream lectorDeObjetos = new ObjectInputStream(flujoEntrada);
+            return (Batalla) lectorDeObjetos.readObject();
+        } catch (IOException ex) {
+            
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(FileManager.class.getName()).log(Level.SEVERE, null, ex);
+        }        
+        return null;        
+    }
+}
